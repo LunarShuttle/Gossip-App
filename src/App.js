d80bdcf6bbac79a3766b2ba27ebcd65bf380e33f
Shridhar-dev <shridharkamat10@gmail.com>
2021-01-27 15:49:47 +0530

    first one

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { createContext, useEffect,useState } from 'react';
 let User = createContext()
 function App() {
   const [logStatus, setLogStatus] = useState()
- 
+  
   useEffect(()=>{
     auth.onAuthStateChanged(user =>{
       if(user){
@@ -32,15 +32,21 @@ function App() {
 
 function Login() {
   const [members, setMembers] = useState([])
-
+  const [check, setCheck] = useState(0)
   useEffect(() => {
+    if(check === 0){
       db.collection('Users').onSnapshot(snap=>{
           setMembers(snap.docs.map((doc)=>({id:doc.id,data:doc.data()})))
          
       })
+    }
   }, [])
 
-  function signIn(){
+   function signIn(){
+    
+
+    setCheck(check+1)
+ 
     auth.signInWithPopup(provider)
     .then(({user}) => {
      
@@ -70,11 +76,11 @@ function Login() {
     .catch(error => alert(error.message))
   }
   return (
-    <div className="d-flex base justify-content-center align-items-center flex-column" style={{backgroundColor:'#2F4F4F',height:'100vh'}} onClick={signIn} >
+    <div className="d-flex base justify-content-center align-items-center flex-column" style={{backgroundColor:'#2F4F4F',height:'100vh'}} >
       <div >
         <img src={Logo} className="img-fluid" alt="app-logo"/>
       </div>
-      <div style={{backgroundColor:'white',borderRadius:'3px',maxWidth:'80%',boxShadow:' 0px 9px 39px -15px rgba(0,0,0,0.75)'}} onClick={signIn} className="signIn-btn container p-2 mt-4 d-flex align-items-center justify-content-center ">
+      <div style={{backgroundColor:'white',borderRadius:'3px',maxWidth:'80%',boxShadow:' 0px 9px 39px -15px rgba(0,0,0,0.75)'}} onClick={()=>{signIn()}} className="signIn-btn container p-2 mt-4 d-flex align-items-center justify-content-center ">
         <div style={{backgroundColor:'white'}}>
           <img src={GLogo} className="img-fluid mr-4 ml-2" width="30" alt="google-logo" />
         </div>
